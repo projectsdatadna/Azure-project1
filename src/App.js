a68e409f2e0ed7eba7f15501a2e0aac76ba92584
karan <karan110102@gmail.com>
2024-02-05 20:39:51 +0530

    new

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'; 
+import 'bootstrap/dist/css/bootstrap.css'; 
+import Form from 'react-bootstrap/Form'; 
+import Button from 'react-bootstrap/Button'; 
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default function App() { 
+return ( 
+	<div style={{ display: 'block', 
+        
+				width: 700, 
+				padding: 30 }}> 
+	<h4>Personal Information</h4> 
+	<Form> 
+	<Form.Group> 
+		<Form.Control type="text"
+						placeholder="Name" /><br></br>
+		</Form.Group> 
+		<Form.Group> 
+		<Form.Control type="email"
+						placeholder="Email" /> <br></br>
+		</Form.Group> 
+		<Form.Group> 
+		<Form.Control type="number" placeholder="Age" /> <br></br>
+		</Form.Group> 
+		<Button variant="primary" type="submit"> 
+		Click here to submit form 
+		</Button> 
+	</Form> 
+	</div> 
+); 
 }
-
-export default App;
